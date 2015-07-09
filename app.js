eda5a3608a602863b95bf9f9f06675f97d0e0a1f
Kevin D Coffey <kevdougful@gmail.com>
2015-07-09 14:37:24 -0500

    adde requires for mongodb and monk

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// mongoDB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/PlanIt');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
