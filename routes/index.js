82f8d0dec0f2d2bcd0f2e16a5bc96170e48c5c31
Kevin D Coffey <kevdougful@gmail.com>
2015-07-10 15:23:01 -0500

    added page stubs

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/newrequest', function(req, res, next) {
-  res.render('newrequest');
+router.get('/requests', function(req, res, next) {
+  res.render('requests');
+});
+
+router.get('/tasks', function(req, res, next) {
+  res.render('tasks');
+});
+
+router.get('/stakeholders', function(req, res, next) {
+  res.render('stakeholders');
 });
 
 module.exports = router;
