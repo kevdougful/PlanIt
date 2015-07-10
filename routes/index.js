f63c9e81af51c0e27b559a8244936b6147bfa798
Kevin D Coffey <kevdougful@gmail.com>
2015-07-10 15:05:20 -0500

    added newrequest route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/newrequest', function(req, res, next) {
+  res.render('newrequest');
+});
+
 module.exports = router;
