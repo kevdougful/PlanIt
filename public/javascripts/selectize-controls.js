2b4740bf508c7586c01cdb8c8dd61cc07b4ad6c6
Kevin Coffey <kevdougful@gmail.com>
2015-07-11 01:29:01 -0500

    pick-person select working, but text white

--- public/javascripts/selectize-controls.js
+++ public/javascripts/selectize-controls.js
@@ -4,3 +4,63 @@ $(function() {
 		sortField: 'text'
 	});
 });
+
+var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
+                  '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
+$('#others').selectize({
+	persist: false,
+	maxItem: null,
+	valueField: 'email',
+	labelField: 'name',
+	searchField: ['name', 'email'],
+	options: [
+		{email: 'kevdougful@gmail.com', name: 'Kevin Coffey'},
+		{email: 'godsdice24@netscape.net', name: 'Chris Coffey'},
+		{email: 'jrvcoffey@gmail.com', name: 'Jessica Coffey'}
+	],
+	render: {
+        item: function(item, escape) {
+            return '<div>' +
+                (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
+                (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+            '</div>';
+        },
+        option: function(item, escape) {
+            var label = item.name || item.email;
+            var caption = item.name ? item.email : null;
+            return '<div>' +
+                '<span class="label">' + escape(label) + '</span>' +
+                (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
+            '</div>';
+        }
+    },
+    createFilter: function(input) {
+        var match, regex;
+
+        // email@address.com
+        regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
+        match = input.match(regex);
+        if (match) return !this.options.hasOwnProperty(match[0]);
+
+        // name <email@address.com>
+        regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
+        match = input.match(regex);
+        if (match) return !this.options.hasOwnProperty(match[2]);
+
+        return false;
+    },
+    create: function(input) {
+        if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
+            return {email: input};
+        }
+        var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
+        if (match) {
+            return {
+                email : match[2],
+                name  : $.trim(match[1])
+            };
+        }
+        alert('Invalid email address.');
+        return false;
+    }
+});
